43b64a705d8b4a0016ad65a31aa9492a038ad74f
Derek Leung <hkz88i00123@gmail.com>
2021-11-08 23:04:59 +0000

    update selenium ,finished

--- Selenium/AutoMessage/AutoMessage.py
+++ Selenium/AutoMessage/AutoMessage.py
@@ -8,33 +8,50 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
-
-
-# variable to store the name of the contact
-contact = "Wahda"
-#variable to store message
-message = "hello Allah"
+import pandas as pd
+import numpy as np
+from datetime import date
+PATH="/Users/howangleung/Desktop/Projects/Python/Selenium/AutoMessage/chromedriver"
 options = webdriver.ChromeOptions()
 options.add_argument("--user-data-dir=chrome-data")
 options.add_experimental_option("detach", True)
-# url = "https://web.whatsapp.com"
-url="https://web.whatsapp.com"
-PATH="/Users/howangleung/Desktop/Projects/Python/Selenium/AutoMessage/chromedriver"
 driver = webdriver.Chrome(PATH,options=options)
-wait = WebDriverWait(driver, 10)
- 
-driver.get(url)
-print("Scan QR Code and press Enter")
-search = wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@role='textbox']")))
-search.click()
-#time.sleep(3)
-#title = 'FLAT 403! '
-title = 'MySelf'
-search.send_keys(title)
-search_result = wait.until(EC.visibility_of_element_located((By.XPATH,f"//*[@title='{title}']")))
-search_result.click()
-textBox = wait.until(EC.visibility_of_element_located((By.XPATH,f"//*[@role='textbox' and @data-tab='9']")))
-textBox.send_keys("Hi")
-textBox.send_keys(Keys.ENTER)
 
-#"//*[@role='textbox']"
+def main():
+    path = "./Rubbish Disposal Schedule.xlsx"
+    message = getFromExcel(path)
+    sendMessage(message)
+
+def getFromExcel(path):
+    df = pd.read_excel(path,index_col=False)
+    df["Date"] = df["Date"].dt.strftime("%d/%m/%y")
+    today = date.today()
+    today = today.strftime("%d/%m/%y")
+    result_mark = df["Date"]==today
+    pos = np.flatnonzero(result_mark)
+    result = df.iloc[pos]
+    message =f"Hi, *{result['Person'][0]}*, today is *{result['Date'][0]}*, this is your turn to take out the trash and to handle the recycling according to the schedule, thank you very much ! _This is an automated message, do not reply/quote._"
+    return message
+
+
+
+def sendMessage(message):
+    url="https://web.whatsapp.com"
+    wait = WebDriverWait(driver, 10)
+    driver.get(url)
+    search = wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@role='textbox']")))
+    search.click()
+    #title = 'FLAT 403! '
+    title = 'MySelf'
+    search.send_keys(title)
+    search_result = wait.until(EC.visibility_of_element_located((By.XPATH,f"//*[@title='{title}']")))
+    search_result.click()
+    textBox = wait.until(EC.visibility_of_element_located((By.XPATH,f"//*[@role='textbox' and @data-tab='9']")))
+    print(message)
+    textBox.send_keys(message)
+    textBox.send_keys(Keys.ENTER)
+    time.sleep(2)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
